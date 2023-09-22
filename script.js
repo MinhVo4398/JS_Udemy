963fd102dabf8f516f663b2eae0c31525d6a608e
MinhVo <minhvo4398@gmail.com>
2023-09-22 08:45:19 +0700

    lession 1

---
+++ script.js
@@ -0,0 +1,25 @@
+let js = "amazing";
+console.log(40 +8 + 23 -10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matidla = 'JM';
+let $function = 27;
+
+let PI = 3.1315;
+
+let myFirstJob = 'Coder';
+let myCurrentJob = 'Teacher';
+
+console.log(myFirstJob);
+
+
+
+
